df3bbb542d6d293384904440281ce687b5fda5b9
Shambhavi Singh <shambhavi.singh@securaa.io>
2026-06-15 22:30:46 +0530

    Increment and Decrement Operators

---
+++ src/operators/IncrementDecrementOperator.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class IncrementDecrementOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5, y=4, z;
+		z=2*x++ + 3 * ++x ;
+		System.out.println(z);
+
+	}
+
+}
